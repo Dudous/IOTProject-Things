300dd8d73ec34e22cf7bc7337f651e17f174010e
Adrian Gobara <adriangobara@gmail.com>
2024-11-26 14:02:15 -0300

    fingers

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Use a service account.
-cred = credentials.Certificate('./park-iot-firebase-config.json')
+cred = credentials.Certificate("C:/Users/DELL/Desktop/IOTProject-Things/park-iot-firebase-adminsdk-8cvie-d594c0b71c.json")
 
 app = firebase_admin.initialize_app(cred)
 
@@ -13,33 +13,35 @@ db = firestore.client()
 
 cards_ref = db.collection('cards')
 
-ser = serial.Serial('COM3', 9600)
+ser = serial.Serial('COM4', 9600)
 print('bora pai')
 
 while True:
     data = ser.readline().decode('utf-8').strip()
 
     if(data):
+        print(data)
         cards = cards_ref.get()
 
-        flag = False
+        cardId = ''
 
         for card in cards:
-            if(card.to_dict().get('card') == data):
-                flag = True
-                cards_ref.document(card.id).update({
-                    'exit' : str(datetime.now())
+            if card.to_dict().get('card') == data and card.to_dict().get('exit') == '':
+                cardId = card.id
+                break
+
+        if cardId:
+            card_doc = cards_ref.document(cardId).get()
+
+            if card_doc.to_dict().get('exit') == '':
+                cards_ref.document(cardId).update({
+                    'exit': str(datetime.now())
                 })
-                print(f'{card.id}: {card.to_dict()}')
+                continue
 
-        if not flag:
-            data = {'card': data,
-                    'enter': str(datetime.now()),
-                    'exit': ''
-                    }
+        data = {'card': data,
+                'enter': str(datetime.now()),
+                'exit': ''
+            }
 
-            cards_ref.add(data)
-
-            time.sleep(1)
-
-        print('ok')
+        cards_ref.add(data)
