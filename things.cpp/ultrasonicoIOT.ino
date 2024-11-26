e3c1fc2445abbfea2a993bcc05cd1177d622e4c0
Dudous <sampaioeeduardo36@gmail.com>
2024-11-26 15:53:31 -0300

    vaga 3 funcionou

--- things.cpp/ultrasonicoIOT.ino
+++ things.cpp/ultrasonicoIOT.ino
@@ -1,31 +1,21 @@
 #include <Ultrasonic.h>
-#include <Servo.h>
-
-Servo servo;
 
 Ultrasonic ultrasonic(4, 5);
 Ultrasonic ultrasonic2(6, 7);
 int distance;
 int distance2;
-
+int distance3;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(11, OUTPUT);
 }
 
 void loop() {
   distance = ultrasonic.read();
-  distance2 = ultrasonic.read();
-  Serial.println("{\"vaga1\": " + (String) distance + ", \"vaga2\": " + (String) distance2 + "}");
-  if(distance < 50)
-  {
-    for(int i=0; i<90; i++)
-    digitalWrite(11, HIGH);
-  }
-  else
-  {
-    digitalWrite(11, LOW);
-  }
+  distance2 = ultrasonic2.read();
+  distance3 = ultrasonic3.read();
+  
+  Serial.println("{\"vaga1\": " + (String) distance + ", \"vaga2\": " + (String) distance2 + ", \"vaga3\": " + (String) distance2 + "}");
+
   delay(500);
 }
