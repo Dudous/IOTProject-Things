1ce4f1ab61c2e0b5bc1d61401307dd4161932ddf
Dudous <sampaioeeduardo36@gmail.com>
2024-11-26 16:08:22 -0300

    vagas ok

--- things.cpp/ultrasonicoIOT.ino
+++ things.cpp/ultrasonicoIOT.ino
@@ -2,6 +2,7 @@
 
 Ultrasonic ultrasonic(4, 5);
 Ultrasonic ultrasonic2(6, 7);
+Ultrasonic ultrasonic2(8, 9);
 int distance;
 int distance2;
 int distance3;
@@ -12,10 +13,12 @@ void setup() {
 
 void loop() {
   distance = ultrasonic.read();
+  delay(30);
   distance2 = ultrasonic2.read();
+  delay(30);
   distance3 = ultrasonic3.read();
   
-  Serial.println("{\"vaga1\": " + (String) distance + ", \"vaga2\": " + (String) distance2 + ", \"vaga3\": " + (String) distance2 + "}");
+  Serial.println("{\"vaga1\": " + (String) distance + ", \"vaga2\": " + (String) distance2 + ", \"vaga3\": " + (String) distance3 + "}");
 
   delay(500);
 }
--- vagas.py
+++ vagas.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials, firestore
 import json
 
 # Inicializa o Firebase
-cred = credentials.Certificate("C:/Users/DELL/Desktop/IOTProject-Things/park-iot-firebase-adminsdk-8cvie-d594c0b71c.json")
+cred = credentials.Certificate("./park-iot-firebase.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -14,24 +14,24 @@ db = firestore.client()
 vagas_ref = db.collection('vagas')
 
 # Configura a porta serial
-ser = serial.Serial('COM4', 9600)  # Ajuste 'COM3' para a porta correta
+ser = serial.Serial('COM3', 9600)
 time.sleep(2)  # Tempo para inicializar a comunicação serial
 
 while True:
     try:
         data = ser.readline().decode('utf-8').strip()
 
-        data = json.loads(data);
+        data = json.loads(data)
 
         print(data)
 
         for vaga, dist in data.items():
             # Lógica para ativar/desativar o campo 'vaga' com base na distância
             if dist < 15:  # Se a distância for menor que 15 cm, ativar a vaga
-                vagas_ref.document(vaga).update({'vaga': True})
+                vagas_ref.document(vaga).update({'vaga': False})
                 print(f'Vaga ativada: {dist} cm')
             else:  # Caso contrário, desativar a vaga
-                vagas_ref.document(vaga).update({'vaga': False})
+                vagas_ref.document(vaga).update({'vaga': True})
                 print(f'Vaga desativada: {dist} cm')
 
         time.sleep(0.5)  # Ajuste o tempo entre as leituras conforme necessário
